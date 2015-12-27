0aaf47144dc59e4b0148e83d2dd0900b7de92f0d
s0ph1e <sonya1912@gmail.com>
2015-12-27 19:48:12 +0200

    Add google analytics

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,29 @@
 require.config({
 	baseUrl: '..',
+	config: {
+		googleAnalytics: {
+			account: 'UA-71738495-1',
+			domain: 'auto'
+		}
+	},
 	paths: {
 		text: '../bower_components/requirejs-plugins/lib/text',
 		json: '../bower_components/requirejs-plugins/src/json',
 		lodash: '../bower_components/lodash/lodash.min',
-		googleMaps: '../bower_components/google-maps/lib/Google.min'
+		googleMaps: '../bower_components/google-maps/lib/Google.min',
+		googleAnalytics: '../bower_components/amd-google-analytics/ga'
 	}
 });
 
 require([
 	'lodash',
+	'googleAnalytics',
 	'googleMaps',
 	'scripts/draw-map'
-], function(_, GoogleMapsLoader, drawMap) {
+], function(_, googleAnalytics, GoogleMapsLoader, drawMap) {
+
 	GoogleMapsLoader.LIBRARIES = ['places'];
 	GoogleMapsLoader.load(drawMap);
+
+	googleAnalytics('send', 'pageview');
 });
